83fd2ee048e2246c579262cc390cbbbb01113fba
prabathchandradasa <prabathchandradasa601@gmail.com>
2025-04-24 16:27:21 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // PayHere SDK එක resolve වීමට JitPack
+    }
+}
+
+rootProject.name = "AndroidEcommerceApp"
+include(":app")
+ 
